b83550f928eaa81896c953bcd0f991fb384b9691
ForMyWEB <idkthename89@gmail.com>
2024-10-14 10:35:33 +0300

    changed structure

--- app/app.py
+++ app/app.py
@@ -6,3 +6,14 @@ from enum import Enum as PyEnum
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
 db = SQLAlchemy(app)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
