a6456e67ae90eac185e2c564ff8a17d6ec5047bd
ForMyWEB <idkthename89@gmail.com>
2024-10-14 10:44:09 +0300

    added base conf

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pub/*.{html,js,css}"],
+  content: ["./app/static/**/*.{html,js,css}"],
   theme: {
+
+    colors:{
+
+    },
+
+    screens: {
+    'sm': '640px',
+    'md': '768px',
+    'lg': '1024px',
+    'xl': '1280px',
+    '2xl': '1536px',
+  },
     extend: {},
   },
   plugins: [],
 }
-
