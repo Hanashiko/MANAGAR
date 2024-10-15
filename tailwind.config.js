17f9fff1a6205ce06fa0aeff2557cbb3a5169cdf
ForMyWEB <idkthename89@gmail.com>
2024-10-15 17:52:21 +0300

    done all needed pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/static/**/*.{html,js,css}"],
+  content: ["./app/templates/**/*.{html,js,css}"],
   theme: {
 
     colors:{
-
+      'nemo': '#7D8AF6',
+      'nemo-main': '#808DF6',
+      'for-buttons': '#FF5A05',
+      'register-bg': '#7B68EE'
     },
 
-    extend: {},
+    extend: {
+      backgroundImage:{
+        'mainpage-gradient': 'radial-gradient(circle, rgba(155,180,255,1) 29%, rgba(84,129,255,1) 96%)',
+        'register-gradient': 'radial-gradient(circle, rgba(46, 67, 240, 0.61) 61%, rgba(63, 83, 244, 0.73) 73%, rgba(102, 119, 255, 1.0) 100%)'
+      }
+    },
   },
   plugins: [],
 }
